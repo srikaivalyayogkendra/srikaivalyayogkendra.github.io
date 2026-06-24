54efbe7d43cd82ccedea7a09d3a0e88efdb21f1e
srikaivalyayogkendra <srishtiyog28@gmail.com>
2026-06-24 11:29:04 +0530

    Add language switch function with alerts

---
+++ js/language.js
@@ -0,0 +1,19 @@
+function switchLanguage(lang){
+
+if(lang === 'hi'){
+
+alert(
+'हिन्दी भाषा संस्करण सक्रिय है।'
+);
+
+}
+
+if(lang === 'en'){
+
+alert(
+'English version will be available soon.'
+);
+
+}
+
+}
